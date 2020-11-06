b3c5fdf2cc23ac9ef1e1368e1f10407ba8700138
Nick On <nick.on437@gmail.com>
2020-11-06 18:13:24 +1100

    New - Basic files structure

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div id="title"></div>
+            <div className="minor-separator" />
+            <ul>
+                <li>About me</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
