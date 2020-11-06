3b6be4e6eb70a5919b88ad59b1ed084ff231a6e4
Nick On <nick.on437@gmail.com>
2020-11-06 20:08:21 +1100

    Improve - Format body with separator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Body from './components/Body';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <div id="body">
+            <div className="body-separator"></div>
+            <div className="body-content"></div>
+        </div>
+    )
+}
+
+export default Body
