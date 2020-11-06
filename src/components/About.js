d1f9f4a5fdc38786b908b828a55cf6015bce13ed
Nick On <nick.on437@gmail.com>
2020-11-06 23:45:13 +1100

    Refactor - Make content in home more responsive

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div id="about" className="body-content">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div id="home" className="body-content">
-            <div>HI,<br></br>I'M NICK</div>
+            <div className="page-title">HI,<br></br>I'M NICK</div>
             <div>A front-end developer who wants to create the optimal experience with intuitive design and scalable applications.</div>
             <ul className="horizontal-list">
                 <li><button className="link">HIRE ME</button></li>
