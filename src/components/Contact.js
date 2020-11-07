54322d0d1d7403e7d4cef0901c56370c8a9db8d3
Nick On <nick.on437@gmail.com>
2020-11-07 18:54:15 +1100

    New - Add contact details

---
+++ src/components/Contact.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Icons from '../resources/values/Icons';
+
+const Contact = () => {
+    return (
+        <div id="contact" className="body-content">
+            <h1>Contact</h1>
+            <ul>
+                <li>
+                    <a href="mailto:nick.on437@gmail.com" className="link">
+                        <img src={Icons.email.logo} alt={Icons.email.alt} />
+                        nick.on437@gmail.com
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/nick-on/" className="link">
+                        <img src={Icons.linkedin.logo} alt={Icons.linkedin.alt} />
+                        linkedin.com/in/nick-on
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/nickon437" className="link">
+                        <img src={Icons.github.logo} alt={Icons.github.alt} />
+                        github.com/nickon437
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Contact
