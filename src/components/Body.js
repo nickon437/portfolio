9376ab3ff75b83ec10335e8cd1bc68342db3f97a
Nick On <nick.on437@gmail.com>
2020-11-07 14:16:28 +1100

    New - Add page switching animation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,50 +1,30 @@
-import React, { useContext } from 'react';
-// import ReactCSSTransitionGroup from 'react-transition-group';
-// import { TransitionGroup, CSSTransition } from "react-transition-group";
-import {
-    CSSTransition,
-    TransitionGroup,
-    SwitchTransition,
-  } from 'react-transition-group';
-import { Switch, Route } from "react-router-dom";
-import Page from '../resources/enum/Page';
+import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { Switch, Route, withRouter } from "react-router-dom";
 import Home from './Home';
 import About from './About';
-import { UiContext } from '../context/UiContext';
 
-const Body = () => {
-    const [ui, setUi] = useContext(UiContext);
-    let content = null;
-
-    // if (ui.ctxPage === Page.Home) {
-    //     content = <Home />;
-    // } else if (ui.ctxPage === Page.About) {
-    //     content = <About />;
-    // }
+const Body = ({ location }) => {
 
     return (
         <div id="body">
             <div className="body-separator" />
-            {/* {content} */}
-            {/* <TransitionGroup component={null}> */}
-            {/* <SwitchTransition> */}
-                {/* <CSSTransition 
-                    transitionName="body-content"
-                    transitionEnterTimeout={500}
-                    transitionLeaveTimeout={300}
-                >   */}
-                    {/* <div>ads</div> */}
-                    {/* {content} */}
-                {/* </CSSTransition> */}
-            {/* </SwitchTransition> */}
-            {/* </TransitionGroup> */}
-
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-            </Switch>
+            <TransitionGroup className="transition-group">
+                <CSSTransition 
+                    key={location.key}
+                    classNames="fade"
+                    timeout={300}
+                >
+                    <section className="route-section">
+                        <Switch location={location}>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/about" component={About} />
+                        </Switch>
+                    </section>
+                </CSSTransition>
+            </TransitionGroup>
         </div>
     )
 }
 
-export default Body
+export default withRouter(Body);
