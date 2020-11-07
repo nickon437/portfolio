40ca28e8800f19c282fe5786c6943bdbd67da590
Nick On <nick.on437@gmail.com>
2020-11-07 11:54:13 +1100

    Refactor - Use Link for all elements in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,13 @@ const Header = () => {
         <div id="header">
             <Link id="name-title" className="link" to="/">NICK . ON</Link>
             <div className="small-separator" />
-            <ul className="horizontal-list nav">
-                {/* <li><button className="link" onClick={handleAbout}>ABOUT ME</button></li> */}
-                <li><Link className="link" to="/about">ABOUT ME</Link></li>
-                <li>.</li>
-                <li>PROJECTS</li>
-                <li>.</li>
-                <li>CONTACT</li>
-            </ul>
+            <nav className="horizontal-list">
+                <Link className="link" to="/about">ABOUT ME</Link>
+                <span>.</span>
+                <Link className="link" to="/projects">PROJECTS</Link>
+                <span>.</span>
+                <Link className="link" to="/contact">CONTACT</Link>
+            </nav>
         </div>
     )
 }
