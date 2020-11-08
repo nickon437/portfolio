d52a97f8248f4a042cee2d5348ca78be8535cc0c
Nick On <nick.on437@gmail.com>
2020-11-08 21:40:26 +1100

    Fix - Resume download issue

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Icons from '../resources/values/Icons';
+import Resume from '../resources/docs/Resume.pdf';
 
 const About = () => {
     const toolData = [
@@ -26,7 +27,7 @@ const About = () => {
         <div id="about" className="body-content">
             <h1>ABOUT ME</h1>
             <div className="page-content">A software engineer who loves to collab and build intuitive, maintainable and scalable applications. I have experienced in working in Agile environment with an emphasis on utilizing best practices to create software that meets requirements and schedule.</div>
-            <div><a href="../assets/docs/Resume.pdf" className="std-btn" download>DOWNLOAD RESUME</a></div>
+            <div><a href={Resume} className="std-btn" download>DOWNLOAD RESUME</a></div>
             <div>
                 <h2>TECHNOLOGIES I WORK WITH</h2>
                 <ul className="tech-stack horizontal-list">{toolList}</ul>
